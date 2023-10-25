d771f570708d949404ff530334b2f990c7ea94a0
KRD <44735306+KRD-Kai@users.noreply.github.com>
2023-10-25 15:56:26 +0100

    Make index fetch only

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,32 +4,32 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <TodoListCard />
+                    <TableDisplay />
                 </Col>
             </Row>
         </Container>
     );
 }
 
-function TodoListCard() {
-    const [items, setItems] = React.useState(null);
+function TableDisplay() {
+    const [items, setItems] = React.useState([]);
 
     React.useEffect(() => {
         fetch('/items')
-            .then(r => r.json())
+            .then((r) => r.json())
             .then(setItems);
     }, []);
 
-    const onNewItem = React.useCallback(
-        newItem => {
-            setItems([...items, newItem]);
+    const onNewItems = React.useCallback(
+        (items) => {
+            setItems(items);
         },
         [items],
     );
 
     const onItemUpdate = React.useCallback(
-        item => {
-            const index = items.findIndex(i => i.id === item.id);
+        (item) => {
+            const index = items.findIndex((i) => i.id === item.id);
             setItems([
                 ...items.slice(0, index),
                 item,
@@ -40,8 +40,8 @@ function TodoListCard() {
     );
 
     const onItemRemoval = React.useCallback(
-        item => {
-            const index = items.findIndex(i => i.id === item.id);
+        (item) => {
+            const index = items.findIndex((i) => i.id === item.id);
             setItems([...items.slice(0, index), ...items.slice(index + 1)]);
         },
         [items],
@@ -51,11 +51,11 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
-            <AddItemForm onNewItem={onNewItem} />
+            <FetchItemsForm onNewItems={onNewItems} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">No todos</p>
             )}
-            {items.map(item => (
+            {items.map((item) => (
                 <ItemDisplay
                     item={item}
                     key={item.id}
@@ -67,109 +67,48 @@ function TodoListCard() {
     );
 }
 
-function AddItemForm({ onNewItem }) {
-    const { Form, InputGroup, Button } = ReactBootstrap;
-
-    const [newItem, setNewItem] = React.useState('');
+function FetchItemsForm({ onNewItems }) {
+    const { Form, Button } = ReactBootstrap;
     const [submitting, setSubmitting] = React.useState(false);
 
-    const submitNewItem = e => {
+    const getItems = (e) => {
         e.preventDefault();
         setSubmitting(true);
-        fetch('/items', {
-            method: 'POST',
-            body: JSON.stringify({ name: newItem }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(r => r.json())
-            .then(item => {
-                onNewItem(item);
+        fetch('/items')
+            .then((r) => r.json())
+            .then((items) => {
+                onNewItems(items);
                 setSubmitting(false);
-                setNewItem('');
             });
     };
 
     return (
-        <Form onSubmit={submitNewItem}>
-            <InputGroup className="mb-3">
-                <Form.Control
-                    value={newItem}
-                    onChange={e => setNewItem(e.target.value)}
-                    type="text"
-                    placeholder="New Item"
-                    aria-describedby="basic-addon1"
-                />
-                <InputGroup.Append>
-                    <Button
-                        type="submit"
-                        variant="success"
-                        disabled={!newItem.length}
-                        className={submitting ? 'disabled' : ''}
-                    >
-                        {submitting ? 'Adding...' : 'Add Item'}
-                    </Button>
-                </InputGroup.Append>
-            </InputGroup>
+        <Form onSubmit={getItems}>
+            <Button
+                type="submit"
+                variant="success"
+                className={submitting ? 'disabled' : ''}
+            >
+                {submitting ? 'Fetching...' : 'Fetch todos'}
+            </Button>
         </Form>
     );
 }
 
-function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
+function ItemDisplay({ item }) {
     const { Container, Row, Col, Button } = ReactBootstrap;
-
-    const toggleCompletion = () => {
-        fetch(`/items/${item.id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                name: item.name,
-                completed: !item.completed,
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(r => r.json())
-            .then(onItemUpdate);
-    };
-
-    const removeItem = () => {
-        fetch(`/items/${item.id}`, { method: 'DELETE' }).then(() =>
-            onItemRemoval(item),
-        );
-    };
 
     return (
         <Container fluid className={`item ${item.completed && 'completed'}`}>
             <Row>
                 <Col xs={1} className="text-center">
-                    <Button
-                        className="toggles"
-                        size="sm"
-                        variant="link"
-                        onClick={toggleCompletion}
-                        aria-label={
-                            item.completed
-                                ? 'Mark item as incomplete'
-                                : 'Mark item as complete'
-                        }
-                    >
-                        <i
-                            className={`far ${
-                                item.completed ? 'fa-check-square' : 'fa-square'
-                            }`}
-                        />
-                    </Button>
+                    
                 </Col>
                 <Col xs={10} className="name">
                     {item.name}
                 </Col>
                 <Col xs={1} className="text-center remove">
-                    <Button
-                        size="sm"
-                        variant="link"
-                        onClick={removeItem}
-                        aria-label="Remove Item"
-                    >
-                        <i className="fa fa-trash text-danger" />
-                    </Button>
+                    
                 </Col>
             </Row>
         </Container>
